3f2deacf8463f78af539121f45b24e5cca552315
Lukas Bischof <me@luk4s.dev>
2022-04-26 21:49:31 +0200

    Provide Some More Tests
    Cool new test

--- Assets/Scripts/Tests/HealthTest.cs
+++ Assets/Scripts/Tests/HealthTest.cs
@@ -1,50 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
-using System.Runtime.Serialization;
-using System.Reflection;
 
-public class Slider : UnityEngine.UI.Slider
+namespace Tests
 {
-}
+    public class Slider : UnityEngine.UI.Slider
+    {
+    }
 
-public class HealthTest
-{
-  [Test]
-  public void TestTakesDamage()
-  {
-    GameObject gameObject = new GameObject("Test");
-    gameObject.AddComponent<Health>();
-    Health healthComponent = gameObject.GetComponent<Health>() as Health;
+    public class HealthTest
+    {
+        [Test]
+        public void TestTakesDamage()
+        {
+            var gameObject = new GameObject("Test");
+            gameObject.AddComponent<Health>();
+            var healthComponent = gameObject.GetComponent<Health>() as Health;
 
-    GameObject sliderObject = new GameObject("Slider");
-    sliderObject.AddComponent<Slider>();
-    healthComponent.HealthBar = sliderObject.GetComponent<Slider>();
+            var sliderObject = new GameObject("Slider");
+            sliderObject.AddComponent<Slider>();
+            healthComponent.HealthBar = sliderObject.GetComponent<Slider>();
 
-    healthComponent.TakeDamage(1);
-    Assert.AreEqual(99, healthComponent.health);
+            healthComponent.TakeDamage(1);
+            Assert.AreEqual(expected: 99, healthComponent.health);
 
-    healthComponent.TakeDamage(2);
-    Assert.AreEqual(97, healthComponent.health);
-  }
+            healthComponent.TakeDamage(2);
+            Assert.AreEqual(97, healthComponent.health);
+        }
 
-  [Test]
-  public void TestDie()
-  {
-		bool hasDied = false;
+        [Test]
+        public void TestDie()
+        {
+            bool hasDied = false;
 
-		GameObject gameObject = new GameObject("Test");
-    gameObject.AddComponent<Health>();
-    Health healthComponent = gameObject.GetComponent<Health>() as Health;
-		healthComponent.OnDied += () => hasDied = true;
+            var gameObject = new GameObject("Test");
+            gameObject.AddComponent<Health>();
+            var healthComponent = gameObject.GetComponent<Health>() as Health;
+            healthComponent.OnDied += () => hasDied = true;
 
-    GameObject sliderObject = new GameObject("Slider");
-    sliderObject.AddComponent<Slider>();
-    healthComponent.HealthBar = sliderObject.GetComponent<Slider>();
+            var sliderObject = new GameObject("Slider");
+            sliderObject.AddComponent<Slider>();
+            healthComponent.HealthBar = sliderObject.GetComponent<Slider>();
 
-    healthComponent.TakeDamage(5000);
-		Assert.AreEqual(true, hasDied);
-  }
+            healthComponent.TakeDamage(5000);
+            Assert.AreEqual(expected: true, actual: hasDied);
+        }
+    }
 }
