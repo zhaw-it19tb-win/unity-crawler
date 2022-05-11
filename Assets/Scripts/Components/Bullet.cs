073047d1b90df2f08eb4def454cabdd667fa2661
Lukas Bischof <me@luk4s.dev>
2022-05-11 20:49:08 +0200

    Fix archer animation

--- Assets/Scripts/Components/Bullet.cs
+++ Assets/Scripts/Components/Bullet.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
   public GameObject bullet;
-
-    // Update is called once per frame
-    private void Update()
-    {
-        Debug.Log("bullet instance ide" + bullet.GetInstanceID()) ;
-    }
+  
     void OnCollisionEnter2D(Collision2D collision) {
    
     if (collision.gameObject.GetComponent<Health>() != null) {
