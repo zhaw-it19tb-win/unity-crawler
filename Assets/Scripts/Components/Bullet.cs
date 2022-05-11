5110c046eae33e20330cef034ad6e7e566233cb4
Lukas Bischof <me@luk4s.dev>
2022-05-11 22:50:31 +0200

    Re-add effect

--- Assets/Scripts/Components/Bullet.cs
+++ Assets/Scripts/Components/Bullet.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject bullet;
-
-  public GameObject bullet;
-  
-    void OnCollisionEnter2D(Collision2D collision) {
-   
-    if (collision.gameObject.GetComponent<Health>() != null) {
-      collision.gameObject.GetComponent<Health>().TakeDamage(5);
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Health>() != null)
+        {
+            collision.gameObject.GetComponent<Health>().TakeDamage(5);
+        }
     }
 }
