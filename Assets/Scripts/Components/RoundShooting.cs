c34a4c0b565fd97cb5c3fdabb12107d56b4d9f77
Lukas Bischof <me@luk4s.dev>
2022-05-11 16:02:19 +0200

    stuff

--- Assets/Scripts/Components/RoundShooting.cs
+++ Assets/Scripts/Components/RoundShooting.cs
@@ -6,24 +6,24 @@ namespace Components
 {
   public class RoundShooting : MonoBehaviour, IBossAttack {
     public Transform firePoint;
+    [FormerlySerializedAs("Target")] public Transform target;
     public GameObject bulletPrefab;
     public float bulletForce = 1f;
     [FormerlySerializedAs("HowMany")] public int howMany;
 
-    private void Start() {
-      var playerObjs = GameObject.FindGameObjectsWithTag("Player");
-    }
-
     public void Shoot() {
+      Debug.Log(firePoint.position);
+      Vector3 modifiedFirePoint = Vector3.MoveTowards(firePoint.position, target.position, 0.5f);
+      
+      
       for (var deg = 0; deg <= 360; deg += 360 / howMany)
       {
         var rad = (deg / 360.0) * Math.PI * 2.0;
-        var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        var bullet = Instantiate(bulletPrefab, modifiedFirePoint, firePoint.rotation);
         var direction = new Vector2((float)Math.Sin(rad), (float)Math.Cos(rad)).normalized;
-        
-        Debug.Log(direction);
-        
-        bullet.GetComponent<Rigidbody2D>().AddForce(direction * bulletForce, ForceMode2D.Impulse);
+
+        var component = bullet.GetComponent<Rigidbody2D>();
+        component.AddForce(direction * bulletForce, ForceMode2D.Impulse);
       }
     }
   }
