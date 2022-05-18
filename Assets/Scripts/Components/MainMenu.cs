acde97b7076f6ae06c57abb6e4c252fa9bd30b8d
jovanni1 <jovanni1@students.zhaw.ch>
2022-05-18 12:14:23 +0200

    Fixes bug 211 for the main menu

--- Assets/Scripts/Components/MainMenu.cs
+++ Assets/Scripts/Components/MainMenu.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private static bool firstCall = true;
+
     /// <summary>
     /// Load the next scene according to build index (see File\Build Settings...). Called by the PlayButton.
     /// </summary>
     public void Play()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     /// <summary>
@@ -17,5 +19,19 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    /// <summary>
+    /// If returning from the pause menu remove the redundant EventSystem.
+    /// </summary>
+    void OnEnable()
+    {
+        if (firstCall)
+        {
+            firstCall = false;
+        } else
+        {
+            Destroy(GameObject.Find("MenuEventSystem"));
+        }
     }
 }
