869679877fb0224bbd4c6210f88d1feb8181d5aa
Stefan Teodoropol <teodoste@students.zhaw.ch>
2022-05-19 02:43:00 +0200

    working enemy spawners

---
+++ Assets/Scripts/SpawnerPOI.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerPOI : MonoBehaviour
+{
+    public Vector3 pos;
+    public int num;
+    public int idx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void Activate()
+    {
+        MapEnemies enemies = GameObject.FindObjectOfType<MapEnemies>();
+        for (int i = 0; i < num; i ++) {
+            Instantiate(enemies.simpleEnemies[idx], pos, Quaternion.identity);
+        }
+    }
+}
